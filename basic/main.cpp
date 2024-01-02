ba6a945b66de938018bff3a6d727952eed9552d6
peder frang <peder.frang@gmail.com>
2024-01-02 23:29:51 +0100

    Push

--- basic/main.cpp
+++ basic/main.cpp
@@ -3,6 +3,8 @@
 // generate to new file gcc -S your_file.c
 #include <stdio.h>
 
+void arrayIndexing(void);
+
 struct projectile {
     char unsigned IsThisOnFire;
     int Damage;
@@ -47,12 +49,24 @@ int main(void) {
     Test.ParticlesPerSecond = 3;
     Test.HowManyCooks = 4;
 
-    // allocate mmory for 40 projectile obje ts
+    // arrayIndexing();
+
+    // accessing members of a struct pointer vs value
+
+    projectile TestPointer2;  // When its referencing a value / an actual thing on the stack
+    TestPointer2.Damage = 5;  // with dot (.)
+    // THese are the same
+    projectile *TestPointer3 = &TestPointer2;
+    TestPointer3->Damage = 6;  // I haven now changed Testpointer2's value to 6
+    printf("%d \n", TestPointer2.Damage);
+}
+
+void arrayIndexing(void) {
+    // allocate mmory for 40 projectile objects
 
     projectile Projectiles[40];  // they are placed in memory with an arbitrary
                                  // number of bytes between them, no logic
                                  // needed to access each index
-
     int whichProjectile = 4;
     Projectiles[whichProjectile];  // array indexing.
     // this is strange, pointers, declarated with * should only be able to store
@@ -71,9 +85,23 @@ int main(void) {
 
     int sizeOfProjectiles = sizeof(Projectiles);              // 16 * 40;
     int sizeOfProjectilePointer = sizeof(ProjectilePointer);  // 8 (64 bits, 8 bits per byte)
-
+    // array indexing and setting values
     Projectiles[30].Damage = 60;
     // THESE ARE THE SAME - what this translates to is ref code below
     (ProjectilePointer + 30)->Damage = 100;  // this is really (ProjectilePointer + 30*sizeof(pointer) to move
     // to the correct memory address, but C already does this automatically
+
+    // to display "what really happens" to jump to the correct memory address
+    // what is happening here is PojectilePointer is casted to a byte pointer
+    // and then the size of a projectile is multiplied by 30 and added to the
+    // address of ProjectilePointer, and then casted back to a projectile pointer
+    ((projectile *)((char *)ProjectilePointer + 30 * sizeof(projectile)))->Damage = 100;
+
+    // explained with multiple operatons below
+    char *BytePointer = (char *)ProjectilePointer;          // set to byte pointer
+    BytePointer = BytePointer + (30 * sizeof(projectile));  // move to correct memory address, counting bytes
+    projectile *Thirty = (projectile *)BytePointer;         // cast back to projectile pointer
+    // field would not be available in the regular byte pointer, but on casting and telling C we know that this memory address holds
+    Thirty->Damage = 100;
+    // our struct, we can cast it and let C chill enusring we are a "pro programmer"
 }
