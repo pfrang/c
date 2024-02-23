ce43c97a25e0b0a355ba4495c57602a2ec90ac4d
peder frang <peder.frang@gmail.com>
2024-02-23 12:02:34 +0100

    somethign

--- low-level-academy/structs-&-unions/structs.c
+++ low-level-academy/structs-&-unions/structs.c
@@ -1,35 +1,33 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 
 #define MAX_EMPLOYEES 1000
 struct mystruct {
-	int i;
-	int c;
+  int i;
+  int c;
 };
 
 struct employee_t {
-	int id;
-	char firstname[64];
-	char lastname[64];
-	float income;
-	bool ismanager;
+  int id;
+  char firstname[64];
+  char lastname[64];
+  float income;
+  bool ismanager;
 };
 
 int main() {
-	struct mystruct mystructs[10];
-	struct employee_t Fred;
-	Fred.income = 100.00;
-	Fred.ismanager = true;
+  struct mystruct mystructs[10];
+  struct employee_t Fred;
+  Fred.income = 100.00;
+  Fred.ismanager = true;
+  // an array of employees
+  struct employee_t employees[10];
+  Fred.income = 3;
+  int i = 0;
+  for (i = 0; i < 10; i++) {
+    employees[i].income = 0;
+    employees[i].ismanager = false;
+  }
 
-	// an array of employees
-	struct employee_t employees[10];
-
-	int i = 0;
-	for(i = 0; i < 10; i++) {
-		employees[i].income = 0;
-		employees[i].ismanager = false;
-	}
-
-	printf("%f\n", employees[10].income);
-
+  printf("%f\n", employees[10].income);
 };
