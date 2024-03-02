3af1a0e041559dc9d20b2a5f1f29a5ee29c3a966
peder frang <peder.frang@gmail.com>
2024-03-02 12:26:52 +0100

    push

---
+++ low-level-academy/11-Multi-Module-Programming/part2-creating-libraries/shared-library-object/libtee/lib.c
@@ -0,0 +1,7 @@
+
+#include <stdio.h>
+
+int function_in_a_library() {
+    printf("Using function in a library =)) LOL \n");
+    return 1;
+}
---
+++ low-level-academy/11-Multi-Module-Programming/part2-creating-libraries/static-library-object/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+#include "libtee/libtee.h"
+
+int main() {
+    function_in_a_library();
+    return 0;
+}
