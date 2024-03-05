d092be9543741e45edc16c76730cc491181cb4ae
peder frang <peder.frang@gmail.com>
2024-03-06 00:19:34 +0100

    Lowlevelacademy
    Start final project

---
+++ low-level-academy/12-db-employees/src/main.c
@@ -0,0 +1,66 @@
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+#include "../include/common.h"
+#include "../include/file.h"
+// #include "common.h"
+// #include "file.h"
+// #include "parse.h"
+//
+// the program can be run with the flag -n filepath or -h
+void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <db file>\n", argv[0]);
+  printf("\t -n - create new db file\n");
+  printf("\t -f - (required) path to database file\n");
+  return;
+}
+
+int main(int argc, char *argv[]) {
+  char *filepath = NULL;
+  bool newfile = false;
+  int c;
+  int dbfd = -1; // so we dont actually use it as a valid file descriptor
+
+  while ((c = getopt(argc, argv, "nf:")) != -1) { // if n or f is added
+    switch (c) {
+    case 'n':
+      newfile = true;
+      break;
+    case 'f':
+      filepath = optarg; // return value from getopt and what the command line
+                         // argument was after flag
+      break;
+    case '?':
+      printf("Unknown option -%c\n", c);
+      break;
+    default:
+      return -1;
+    }
+  }
+
+  if (filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+    return 0;
+  }
+
+  if (newfile) {
+    dbfd = create_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      perror("Unable to create db file\n");
+      return -1;
+    }
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      perror("Unable to open db file\n");
+      return -1;
+    }
+  }
+
+  printf("Newfile %d\n", newfile);
+  printf("Filepath: %s\n", filepath);
+
+  return 0;
+}
--- low-level-academy/miscellanous/libraries/src/main.c
+++ low-level-academy/miscellanous/libraries/src/main.c
@@ -13,53 +13,53 @@ void flushStdin(void);
 int read_from_file(int fd, char *output);
 
 int main(int argc, char *argv[]) {
-  if (argc != 2) {
-    printf("Usage %s <filename>: \n", argv[0]);
-    return 0;
-  }
-
-  int fd = open_file_rw(argv[1]);
-
-  if (fd == -1) {
-    return -1;
-  }
-
-  int choice;
-  printf("Select an opion:\n");
-  printf("1. Read file\n");
-  printf("2. Write to file:\n");
-  scanf("%d", &choice);
-  flushStdin();
-
-  switch (choice) {
-  case 1: {
-    char *output = malloc(100);
-    int read = read_from_file(fd, output);
-    free(output);
-    break;
-  }
-  case 2: {
-    char *input = malloc(100);
-    printf("What do you want to write to file %s \n", argv[1]);
-    fgets(input, 100, stdin);
-    int writd = write_to_file(fd, input);
-    if (writd == -1) {
-      return -1;
+    if (argc != 2) {
+        printf("Usage %s <filename>: \n", argv[0]);
+        return 0;
     }
-    free(input);
-    break;
-  default:
-    printf("Illegal number %d", choice);
-    return -1;
-  }
-  }
 
-  close(fd);
-  return 0;
+    int fd = open_file_rw(argv[1]);
+
+    if (fd == -1) {
+        return -1;
+    }
+
+    int choice;
+    printf("Select an opion:\n");
+    printf("1. Read file\n");
+    printf("2. Write to file:\n");
+    scanf("%d", &choice);
+    flushStdin();
+
+    switch (choice) {
+        case 1: {
+            char *output = malloc(100);
+            int read = read_from_file(fd, output);
+            free(output);
+            break;
+        }
+        case 2: {
+            char *input = malloc(100);
+            printf("What do you want to write to file %s \n", argv[1]);
+            fgets(input, 100, stdin);
+            int writd = write_to_file(fd, input);
+            if (writd == -1) {
+                return -1;
+            }
+            free(input);
+            break;
+        }
+        default:
+            printf("Illegal number %d", choice);
+            return -1;
+    }
+
+    close(fd);
+    return 0;
 }
 
 void flushStdin() {
-  int c;
-  while ((c = getchar()) != '\n' && c != EOF) {
-  };
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF) {
+    };
 }
